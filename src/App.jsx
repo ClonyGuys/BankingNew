88df891b494dc7ad37be619c12b2802c1d6fe38b
unknown <arunagunisetti@gmail.com>
2024-05-30 14:47:00 +0530

    nothig

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ function App() {
 
   return (
     <>
-      <h1 className="text-center">Net Banking</h1>
+    <body class='text-center'>
+      
+ 
+     <div class="input-div  ">   
+         <h1 class="pt-4 mt-5 px-5" Style="color: #ff00fd;">AM I ur frnd🤔?</h1>
+    </div>
+    <div class="btns-container my-5 ">
+        <button onclick="yes()"class='me-4 yesbtn'>YES😘</button>
+        <button  class="nobtn">No😡</button>
+    </div>
+    </body>
     </>
   )
 }
